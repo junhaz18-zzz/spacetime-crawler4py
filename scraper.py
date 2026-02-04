e899196d07cb1545fc22187baa4822d88b50ccdf
junhaz18-zzz <junhaz18@uci.edu>
2026-02-03 17:20:08 -0800

    Update scraper.py by Deleting old is_valid function

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import re
 from urllib.parse import urlparse, urljoin
 from html.parser import HTMLParser
+from validator import is_valid
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -40,28 +41,3 @@ def extract_next_links(url, resp):
     except Exception:
         # If lxml crashes or content is binary trash
         return list()
-
-def is_valid(url):
-    # Decide whether to crawl this url or not. 
-    # If you decide to crawl it, return True; otherwise return False.
-    # There are already some conditions that return False.
-    try:
-        parsed = urlparse(url)
-        if parsed.scheme not in set(["http", "https"]):
-            return False
-        return not re.match(
-            r".*\.(css|js|bmp|gif|jpe?g|ico"
-            + r"|png|tiff?|mid|mp2|mp3|mp4"
-            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
-            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
-            + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
-            + r"|epub|dll|cnf|tgz|sha1"
-            + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
-
-    except TypeError:
-        print ("TypeError for ", parsed)
-        raise
-
-
-
